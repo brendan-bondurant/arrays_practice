721c9f2a1ad5c844198b187422dd5aff2ddf1ae9
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-08-02 06:09:21 -0600

    Show four different methods

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,7 @@ arbitrary_numbers = [6, 234, 87, 18938]
 temperatures = [98.6, 94.2, 100.3, 55.9]
 answers = [true, true, false, true]
 
+p places.pop
+p arbitrary_numbers.shift
+p temperatures.unshift(78.0)
+p answers.push(false)
